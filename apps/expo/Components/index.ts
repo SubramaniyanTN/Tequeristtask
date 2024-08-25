18c7d7f9456cc0a0aa76581d967b4900483e05c5
subramaniyan <subramaniyanm@xbi4.com>
2024-08-25 11:41:58 +0530

    added the basic setup for kanban board

--- apps/expo/Components/index.ts
+++ apps/expo/Components/index.ts
@@ -1,1 +1,2 @@
 export { default as SafeAreaViewWrapper } from './SafeAreaViewWrapper'
+export { default as Button } from './Button/Button'
--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,13 +1,98 @@
-import { Text } from '@my/ui'
+import { Button, Text, View } from '@my/ui'
 import { SafeAreaViewWrapper } from '../Components'
 import { GestureDetector, Gesture } from 'react-native-gesture-handler'
+import { CardModel, ColumnModel, KanbanBoard } from '@intechnity/react-native-kanban-board'
+
+enum BOARD_VALUES {
+  notStarted = 'notStarted',
+  inProgress = 'inProgress',
+  completed = 'completed',
+}
 
 export default function Screen() {
   const gesture = Gesture.Pan()
+  const columns = [
+    new ColumnModel(BOARD_VALUES.notStarted, 'Not Started', 1),
+    new ColumnModel(BOARD_VALUES.inProgress, 'In Progress', 2),
+    new ColumnModel(BOARD_VALUES.completed, 'Completed', 3),
+  ]
+
+  const cards = [
+    new CardModel(
+      'card1',
+      BOARD_VALUES.notStarted,
+      '1st Card',
+      'Example card',
+      'test description',
+      [
+        {
+          text: 'Tag1',
+          backgroundColor: '#00FF00',
+          textColor: '#000000',
+        },
+      ],
+      null,
+      1
+    ),
+    new CardModel(
+      'Card2',
+      BOARD_VALUES.notStarted,
+      '2st Card',
+      'Example card',
+      'test description',
+      [
+        {
+          text: 'Tag1',
+          backgroundColor: '#00FF00',
+          textColor: '#000000',
+        },
+      ],
+      null,
+      1
+    ),
+    // ... add more cards ...
+  ]
+  const onCardDragEnd = (
+    srcColumn: ColumnModel,
+    destColumn: ColumnModel,
+    item: CardModel,
+    targetIdx: number
+  ) => {
+    // Handle card drag and drop
+    console.log('On card drag end', { srcColumn, destColumn, item, targetIdx })
+  }
+
+  const onCardPress = (item: CardModel) => {
+    // Handle card press
+    console.log({ item })
+  }
+  const onPress = () => {}
   return (
     <SafeAreaViewWrapper>
+      <View
+        style={{
+          display: 'flex',
+          width: '90%',
+          alignSelf: 'center',
+          justifyContent: 'flex-end',
+          alignItems: 'flex-end',
+        }}
+      >
+        <Button
+          onPress={onPress}
+          variant="outlined"
+          style={{ width: '45%', backgroundColor: '#FFFFFF' }}
+          children="Press me"
+        />
+      </View>
       <GestureDetector gesture={gesture}>
-        <Text>Hello world</Text>
+        <KanbanBoard
+          columns={columns}
+          cards={cards}
+          onDragEnd={onCardDragEnd}
+          onCardPress={onCardPress}
+          style={{}}
+        />
       </GestureDetector>
     </SafeAreaViewWrapper>
   )
